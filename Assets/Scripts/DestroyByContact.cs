e1965d529f6a6780e97e82c77c0ad644d419f511
Timothy Stapleton <retrocede@outlook.com>
2018-11-08 07:58:56 -0700

    start on asteroids

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+	{
+		// being inside game bounds doesn't count
+		if (other.CompareTag("Boundary"))
+		{
+			return;
+		}
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+	}
+}
